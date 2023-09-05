68c6f89eed4dc565211a8e63d263a14c09e44cb2
Daniela Xavier <danielamxsoares@gmail.com>
2023-09-05 18:31:23 -0300

    criar arquivo base do projeto

---
+++ gerenciadorAtividades/main.js
@@ -0,0 +1,25 @@
+const input = require("readline-sync");
+const clear = require("clear");
+
+/**
+ * PR - 01:
+ * Criar objeto de "atividade"(codigoAtividade, descricaoAtividade, dataDeEntrega,
+ * status (Enum: A_FAZER, ANDAMENTO, CONCLUIDA))}
+ */
+
+//
+
+const atividade = [atividade1, atividade2];
+let loop = true;
+
+//PR - 02: Criar método para crud: listar todas as atividades
+//PR - 03: Criar método para crud: cadastrar todas as atividades
+//PR - 04: Criar método para crud: buscar todas as atividades de acordo com status
+//PR - 05: Criar método para crud: alterar atividades
+//PR - 06: Criar método para crud: remover atividades
+
+while (true) {
+  //PR - 07: Criar menu com as opções descritas acima e estrutura do switch de acordo com o menu
+}
+
+//PR - 08: Criar um arquivo README.md legal com a apresentação do projeto
