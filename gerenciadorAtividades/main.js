a82096c815a088da3732c4df9fae3d742edf4191
Erica  Lira <erica.ribeiro@hotmail.com>
2023-09-10 16:47:34 -0300

    PR-07: Menu feito

--- gerenciadorAtividades/main.js
+++ gerenciadorAtividades/main.js
@@ -19,10 +19,18 @@ let busca; // alteração para deletar depois
 //PR - 05: Criar método para crud: alterar atividades
 //PR - 06: Criar método para crud: remover atividades
 
+//PR - 07: Criar menu com as opções descritas acima e estrutura do switch de acordo com o menu
 while (true) {
-  console.log("minha branch 07");
+  console.log("=== GERENCIADOR DE TAREFAS ===");
+  console.log("=========MENU========");
+  console.log("0 - Sair do sistema");
+  console.log("1 - Listar todas as atividades");
+  console.log("2 - Cadastrar uma atividade");
+  console.log("3 - Buscar uma atividade");
+  console.log("4 - Alterar uma atividade");
+  console.log("5 - Remover uma atividade");
+  console.log("=======================");
   
-  //PR - 07: Criar menu com as opções descritas acima e estrutura do switch de acordo com o menu
-}
+  }
 
 //PR - 08: Criar um arquivo README.md legal com a apresentação do projeto
